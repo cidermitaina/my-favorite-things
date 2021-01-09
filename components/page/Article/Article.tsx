d38994676e95aeb53df96463012afa658ffd79bb
cidermitaina <cidermitaina@gmail.com>
2021-01-09 19:00:09 +0900

    feat: add return button and refactor

--- components/page/Article/Article.tsx
+++ components/page/Article/Article.tsx
@@ -1,9 +1,9 @@
 
 import React from 'react'
 import Head from 'next/head'
-
 import styles from './Article.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Article = () => {
   return (
@@ -12,29 +12,37 @@ export const Article = () => {
         <title>Top</title>
       </Head>
 
-      <main className={styles.article}>
-        <div className={styles.left}>
-          <h1 className={styles.title}>Aesopのルームスプレー</h1>
-          <div className={styles.mv}>
-            <Image
-              src="/images/aesop.jpg"
-              alt="Picture of the author"
-              width={640}
-              height={480}
-            />
+      <main className={styles.wrapper}>
+        <Link href="/" >
+          <a className={styles.leftLink}></a>
+        </Link>
+
+        <article className={styles.article}>
+          <div className={styles.left}>
+            <div className={styles.titleArea}>
+              <h1 className={styles.title}>Aesopのルームスプレー</h1>
+            </div>
+            <div className={styles.mv}>
+              <Image
+                src="/images/aesop.jpg"
+                alt="Picture of the author"
+                width={640}
+                height={480}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.right}>
-          <p className={styles.contener}>
-            aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。
-          </p>
-        </div>
-      </main>
-      <div className={styles.arrowLinkArea}>
+          <div className={styles.right}>
+            <p className={styles.contener}>
+              aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。aesopのルームスプレーは本当におすすめです。
+            </p>
+          </div>
+        </article>
+        <div className={styles.arrowLinkArea}>
           <a href="" className={styles.leftLink}></a>
           <p className={styles.number}>1</p>
           <a href="" className={styles.rightLink}></a>
-      </div>
+        </div>
+      </main>
     </>
   )
 }
